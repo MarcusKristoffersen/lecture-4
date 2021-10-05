af8ad9035cb16276aab9280f15335f7e2bb3ecd5
MarcusKri <marcus-kristoffersen@outlook.com>
2021-10-05 20:50:53 +0200

    First test green ð

--- src/test/java/no/kristiania/http/HttpClientTest.java
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -7,6 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class HttpClientTest {
     @Test
     void ShouldDoSomething() {
-        assertEquals(200, 100+200);
+        assertEquals(200, 100+100);
     }
 }

19cb6c0e1dd28fdd7ee05c567a0676a198c405f4
MarcusKri <marcus-kristoffersen@outlook.com>
2021-10-05 19:56:45 +0200

    first test

---
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -0,0 +1,12 @@
+package no.kristiania.http;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpClientTest {
+    @Test
+    void ShouldDoSomething() {
+        assertEquals(200, 100+200);
+    }
+}

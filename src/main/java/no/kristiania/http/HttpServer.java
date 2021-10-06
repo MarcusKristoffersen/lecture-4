3b0cf6dc729566f8b1672f993e91f2d8de17f747
MarcusKri <72551700+MarcusKri@users.noreply.github.com>
2021-10-06 12:53:07 +0200

    Lecture 05 (#2)
    * HttpServer with main method
    
    * shouldRespondWithRequestTargetIn404 red
    
    * shouldRespondWithRequestTargetIn404 passed (green)
    
    * shouldRespondWith200ForKnownRequestTarget failed (red)
    
    * shouldRespondWith200ForKnownRequestTarget passed (GREEN)
    
    * HttpServer main uses HttpServer
    
    * Server should return content type
    
    * Server shouldServeFiles failed (RED)
    
    * Server shouldServeFiles passed (GREEN)
    
    * Lecture finished

--- src/main/java/no/kristiania/http/HttpServer.java
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -3,31 +3,120 @@ package no.kristiania.http;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class HttpServer {
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8080); //Localhost:8080
-
-        Socket clientSocket = serverSocket.accept();
 
 
-        String requestLine = HttpClient.readLine(clientSocket);
+    private final ServerSocket serverSocket;
+    private Path rootDirectory;
 
-        System.out.println(requestLine);
+    public HttpServer(int serverPort) throws IOException {
+        serverSocket = new ServerSocket(serverPort);
 
-        String headerLine;
-        while (!(headerLine = HttpClient.readLine(clientSocket)).isBlank()) {
-            System.out.println(headerLine);
-        } //18-20 Skriver ut headerLinen til porten.
+        new Thread(this::handleClients).start();
+    }
 
-        String response = "<h1>Hello world</h1>";
+    private void handleClients() {
+        try {
+            Socket clientSocket = serverSocket.accept();
 
-        clientSocket.getOutputStream().write((
-                "HTTP/1.1 200 OK\r\n" +
-                        "Content-Length: " + response.length() + "\r\n" +
-                        "Connection: close \r\n" +
+            String[] requestLine = HttpClient.readLine(clientSocket).split(" ");
+            String requestTarget = requestLine[1];
+
+            if (requestTarget.equals("/hello")) {
+                String responseText = "Hello world";
+
+                String response = "HTTP/1.1 200 OK\r\n" +
+                        "Content-Length: " + responseText.length() + "\r\n" +
+                        "Content-Type: text/html\r\n" +
                         "\r\n" +
-                        response
-        ).getBytes());
+                        responseText;
+                clientSocket.getOutputStream().write(response.getBytes());
+            } else {
+                if (rootDirectory != null && Files.exists(rootDirectory.resolve(requestTarget.substring(1)))) {
+                    String responseText = Files.readString(rootDirectory.resolve(requestTarget.substring(1)));
+
+                    String response = "HTTP/1.1 200 OK\r\n" +
+                            "Content-Length: " + responseText.length() + "\r\n" +
+                            "Content-Type: text/html\r\n" +
+                            "\r\n" +
+                            responseText;
+                    clientSocket.getOutputStream().write(response.getBytes());
+                    return;
+                }
+
+
+
+                String responseText = "File not found: " + requestTarget;
+
+                String response = "HTTP/1.1 404 Not found\r\n" +
+                        "Content-Length: " + responseText.length() + "\r\n" +
+                        "\r\n" +
+                        responseText;
+                clientSocket.getOutputStream().write(response.getBytes());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        HttpServer httpServer = new HttpServer(1997); //localhost:1997/hello
+        httpServer.setRoot(Paths.get("."));
+        ////Sette opp en webserver
+        ////Kommenterer ut dette siden jeg har lagd en serverSocket aka website.
+//        ServerSocket serverSocket = new ServerSocket(1997);
+//
+//        Socket clientSocket = serverSocket.accept();
+//
+//        String html = "Hello World";
+//        String contentType = "text/html";
+//
+//        String response = "HTTP/1.1 200 Det gikk bra aka OK fra sia\r\n" +
+//                "Content-Length: " + html.getBytes().length + "\r\n" +
+//                "Content-Type: " + contentType + "\r\n" +
+//                "Connection: close\r\n" +
+//                "\r\n" +
+//                html;
+
+        //clientSocket.getOutputStream().write(response.getBytes());
+        ////Kommenterer ut dette siden jeg har lagd en serverSocket.
+        ////Sette opp en webserver finito
+
+//        ServerSocket serverSocket = new ServerSocket(8080); //Localhost:8080
+//
+//        Socket clientSocket = serverSocket.accept();
+//
+//
+//        String requestLine = HttpClient.readLine(clientSocket);
+//
+//        System.out.println(requestLine);
+//
+//        String headerLine;
+//        while (!(headerLine = HttpClient.readLine(clientSocket)).isBlank()) {
+//            System.out.println(headerLine);
+//        } //18-20 Skriver ut headerLinen til porten.
+//
+//        String response = "<h1>Hello world</h1>";
+//
+//        clientSocket.getOutputStream().write((
+//                "HTTP/1.1 200 OK\r\n" +
+//                        "Content-Length: " + response.length() + "\r\n" +
+//                        "Connection: close \r\n" +
+//                        "\r\n" +
+//                        response
+//        ).getBytes());
+    }
+
+    public int getPort() {
+        return serverSocket.getLocalPort();
+    }
+
+    public void setRoot(Path rootDirectory) {
+
+        this.rootDirectory = rootDirectory;
     }
 }
---
+++ src/test/java/no/kristiania/http/HttpServerTest.java
@@ -0,0 +1,52 @@
+package no.kristiania.http;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpServerTest {
+
+    @Test
+    void shouldReturn404ForUnknownRequestTarget() throws IOException {
+        HttpServer server = new HttpServer(10001);
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/non-existing");
+        assertEquals(404, client.getStatusCode());
+    }
+
+    @Test
+    void shouldRespondWithRequestTargetIn404() throws IOException {
+        HttpServer server = new HttpServer(10002);
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/non-existing");
+        assertEquals("File not found: /non-existing", client.getMessageBody());
+    }
+
+    @Test
+    void shouldRespondWith200ForKnownRequestTarget() throws IOException {
+        HttpServer server = new HttpServer(10004);
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/hello");
+        assertAll(
+                () -> assertEquals(200, client.getStatusCode()),
+                () -> assertEquals("text/html", client.getHeader("Content-Type")),
+                () -> assertEquals("Hello world", client.getMessageBody())
+        );
+    }
+
+    @Test
+    void shouldServeFiles() throws IOException {
+        HttpServer server = new HttpServer(0);
+        server.setRoot(Paths.get("target/test-classes"));
+
+        String fileContent = "A file created at " + LocalTime.now();
+        Files.write(Paths.get("target/test-classes/example-file.txt"), fileContent.getBytes());
+
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/example-file.txt");
+        assertEquals(fileContent, client.getMessageBody());
+    }
+    // Opptak 5, sluttet på 20 min igjen. Skulle gjøre om en index.html til å lese index.txt
+}

2e7153a88d92799fc92240e9f3dc1f5562373318
MarcusKri <marcus-kristoffersen@outlook.com>
2021-10-26 18:19:56 +0200

    RoleDaoTest compiles but fails

--- src/main/java/no/kristiania/person/RoleDao.java
+++ src/main/java/no/kristiania/person/RoleDao.java
@@ -1,18 +1,40 @@
 package no.kristiania.person;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RoleDao {
 
+    private DataSource dataSource;
+
     public RoleDao(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     public void save(String roleName) {
 
     }
 
-    public List<String> listAll() {
-        return null;
+    public List<String> listAll() throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("select * from roles")) {
+                try (ResultSet rs = statement.executeQuery()) {
+                    while (rs.next()) {
+
+                }
+                    ArrayList<String> result = new ArrayList<>();
+                    while (rs.next()) {
+                        result.add(rs.getString("name"));
+                    }
+
+                    return result;
+                }
+             }
+        }
     }
 }
